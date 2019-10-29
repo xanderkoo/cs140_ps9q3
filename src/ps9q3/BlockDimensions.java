c020defde17c0ca6e41da3b48336bce42f4ade2c
xanderkoo <xanderkoo@gmail.com>
2019-10-29 16:49:36 -0700

    fixed compareto

--- src/ps9q3/BlockDimensions.java
+++ src/ps9q3/BlockDimensions.java
@@ -46,12 +46,25 @@ public class BlockDimensions implements Comparable<BlockDimensions> {
 	}
 
 	/**
-	 * @return Compares block dimension objects. -1 if this length is greater than
-	 *         other length, 0 if equal, 1 if less than
+	 * Compares block dimension objects by length, then by width if equal.
+	 * 
+	 * @return -1 if this length is greater than other length, 0 if equal, 1 if less
 	 */
 	public int compareTo(BlockDimensions bd) {
-		// TODO: make it secondarily compare by width, so that we end up with a sorted
-		// list where "larger" elements can't be stacked on smaller ones
-		return ((Integer) this.length).compareTo(bd.length());
+		// TODO: double check to see if this is correct
+		int comp = ((Integer) this.length).compareTo(bd.length());
+		return (comp == 0) ? ((Integer) this.width).compareTo(bd.width()) : comp;
+	}
+
+	/**
+	 * Checks if this block can stack on another block with given dimensions. Note:
+	 * different orientations are not accounted for because all possible block
+	 * orientations will be included in the data.
+	 * 
+	 * @param bd dimensions of the other block
+	 * @return true if can, false if can't
+	 */
+	public boolean canStackOnTop(BlockDimensions bd) {
+		return (this.length < bd.length() && this.width < bd.length());
 	}
 }
